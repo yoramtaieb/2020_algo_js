9c31a9ac35a7b4f8e2f08cbb9c3e7aecea7d8f04
yoramtaieb <taieb.yoram@gmail.com>
2020-03-13 12:33:02 +0100

    exo02 réalisé

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ if (isLogin = true) {
 } else {
     document.write("Not Authorized")
 }
+
+let a = 8;
+b = a + 1;
+a++;
+a *= 2;
+b /= 5;
+b *= b;
+a += 3;
+if (a = 1) {
+    console.log(a += 2)
+} else {
+    console.log(a += 3)
+}
